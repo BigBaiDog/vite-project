54ddf5b8d57422bd09fd3246337ff6e7c8e5d1ad
yuan <1194423126@qq.com>
2025-05-12 22:21:44 +0800

    实现打字效果

---
+++ src/components/Editor.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import TopBar from './TopBar.vue';
+import Tset from './tset.vue';
+
+
+</script>
+
+<template>
+    <div>
+        <TopBar></TopBar>
+        <Tset></Tset>
+    </div>
+</template>
+
+<style scoped lang="less">
+div {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: #263238;
+}
+</style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <header class="top-bar">
+        <a href="javascript:;" class="close"></a>
+        <a href="javascript:;" class="minimize"></a>
+        <a href="javascript:;" class="maximize"></a>
+    </header>
+
+</template>
+
+<style scoped lang="less">
+header {
+    // position: absolute;
+    // top: 0;
+    // left: 0;
+    width: 100%;
+    height: 16px;
+    padding: 16px;
+
+    >a {
+        float: left;
+        display: block;
+        width: 16px;
+        height: 16px;
+        margin-right: 8px;
+        border-radius: 8px;
+
+        &.close {
+            background-color: #FF5F56;
+        }
+
+        &.minimize {
+            background-color: #FFBD2E;
+        }
+
+        &.maximize {
+            background-color: #27C93F;
+        }
+
+    }
+}
+</style>
